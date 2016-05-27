f86f298ee509bc32801e5ffe5794d0a497db5e45
Shdrvsk <sharykin.van@gmail.com>
2016-05-27 12:53:04 +0300

    Исправлены абсолютные пути и имя базы данных

--- models/db.php
+++ models/db.php
@@ -1,7 +1,7 @@
 <?php
     function db_connect() {
         mysql_connect('localhost', 'root', '') or die('No connection with database');
-        mysql_select_db('posts') or die('No database');
+        mysql_select_db('blog') or die('No database');
     }
 
     function db_query($sql) {
