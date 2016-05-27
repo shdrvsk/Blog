2f46ee9f3517c7d744527c69053b6a5518d52c4f
Shdrvsk <sharykin.van@gmail.com>
2016-05-27 14:58:00 +0300

    Реализован механизм удаления статьи

--- article.php
+++ article.php
@@ -1,7 +1,12 @@
 <?php
   require_once __DIR__ . "/models/article.php";
+  require_once __DIR__ . "/models/delete.php";
 
   session_start();
   db_connect();
   $article = get_article($_GET['id']);
   include __DIR__ . "/views/article.php";
+  if(isset($_GET['del_id'])) {
+    delete_article($_GET['del_id']);
+    header('Location: index.php');
+  }
--- views/article.php
+++ views/article.php
@@ -2,4 +2,5 @@
     <h1><?=$article['topic']?></h1>
     <i><?=$article['dt']?></i>
     <p><?=$article['text']?></p>
+    <a href=<?="article.php?del_id={$article['id']}"?>>Delete post</a>
 </html>
