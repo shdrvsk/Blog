adb745644ad43600548dabcb64c996f60efea38d
Shdrvsk <sharykin.van@gmail.com>
2016-05-27 12:49:57 +0300

    Добавлено подключение к базе данных и получение массива статей

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shadrovsky
- * Date: 27.05.2016
- * Time: 12:01
- */
+    require_once __DIR__ . 'models/db.php';
+    require_once __DIR__ . 'models/add.php';
+    require_once __DIR__ . 'models/delete.php';
+    require_once __DIR__ . 'models/edit.php';
+    require_once __DIR__ . 'models/article.php';
+    require_once __DIR__ . 'models/articles.php';
+
+    db_connect();
+    add_article('Topic 1', 'Hello, my friends!');
+    $articles = get_articles();
+    include 'views/articles.php';
