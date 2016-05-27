b4cfc319fd31194266aa606a6dcb63e532f29f14
Shdrvsk <sharykin.van@gmail.com>
2016-05-27 12:15:26 +0300

    Добавлена функция для отправки SQL запросов

--- models/db.php
+++ models/db.php
@@ -3,3 +3,8 @@
         mysql_connect('localhost', 'root', '') or die('No connection with database');
         mysql_select_db('posts') or die('No database');
     }
+
+    function db_query($sql) {
+        $result = mysql_query($sql);
+        return $result;
+    }
