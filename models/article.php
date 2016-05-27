2a0704b257782a9e49f96267ae00bbefc2341004
Shdrvsk <sharykin.van@gmail.com>
2016-05-27 14:39:42 +0300

    Исправлена ошибка в получении статьи по ID

--- models/article.php
+++ models/article.php
@@ -1,4 +1,7 @@
 <?php
+    require_once __DIR__ . '/db.php';
     function get_article($id) {
-        return db_query("SELECT * FROM posts WHERE id='$id'");
+        $resource = db_query("SELECT * FROM posts WHERE id='$id'");
+        $row = mysql_fetch_assoc($resource);
+        return $row;
     }
