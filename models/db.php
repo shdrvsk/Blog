888e1eff28315f2157cb2648bd4fd16f9f343cff
Shdrvsk <sharykin.van@gmail.com>
2016-05-30 11:38:24 +0300

    Добавлен класс базы данных (переход на ООП)

--- models/db.php
+++ models/db.php
@@ -1,10 +1,49 @@
 <?php
-    function db_connect() {
-        mysql_connect('localhost', 'root', '') or die('No connection with database');
-        mysql_select_db('blog') or die('No database');
+
+class Database {
+    private $hostname = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db_name = 'blog';
+    private $link;
+
+    public function __construct() {
+        $this->link = mysqli_connect($this->hostname, $this->username, $this->password, $this->db_name) or die('No connection with database');
     }
 
-    function db_query($sql) {
-        $result = mysql_query($sql);
-        return $result;
+    public function add($dt, $topic, $text) {
+        $sql = "INSERT INTO posts (dt, topic, text) VALUES ('$dt', '$topic', '$text')";
+        $result = mysqli_query($this->link, $sql);
+        if(!$result)
+            die(mysqli_error($this->link));
     }
+
+    public function edit($id, $new_topic, $new_text) {
+        $sql = "UPDATE posts SET topic='$new_topic', text='$new_text' WHERE id='$id'";
+        $result = mysqli_query($this->link, $sql);
+        if(!$result)
+            die(mysqli_error($this->link));
+    }
+
+    public function delete($id) {
+        $sql = "DELETE FROM posts WHERE id='$id'";
+        mysqli_query($this->link, $sql);
+    }
+
+    public function getById($id) {
+        $resource = mysqli_query($this->link, "SELECT * FROM posts WHERE id='$id'");
+        $row = mysqli_fetch_assoc($resource);
+        return $row;
+    }
+
+    public function get() {
+        $resource = mysqli_query($this->link, 'SELECT * FROM posts');
+        $n = mysqli_num_rows($resource);
+        $arr = array();
+        for($i = 1; $i < $n; $i++) {
+            $row = mysqli_fetch_assoc($resource);
+            $arr[] = $row;
+        }
+        return $arr;
+    }
+}
